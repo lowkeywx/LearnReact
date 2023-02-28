a61f3052d8a8dd3f3a49a742df101fbb96797fa3
lowkey <xinghao.wang@tosee.cn>
2023-02-28 02:53:09 +0000

    3

--- src/index.js
+++ src/index.js
@@ -1,33 +1,60 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-function Warning(p) {
-    if(p.t >= 100) {
-        return <p>kai le</p>
-    }
-    return <p>mei kai</p>
+const tempName = {
+    C: "C",
+    F: "F"
 }
 
-class Caculat extends React.Component {
+class TempInput extends React.Component {
     constructor(p) {
         super(p);
         this.state = {
-            t:""
+
+        }
+    }
+    render() {
+        return <fieldset>
+            <legend>this is {this.props.typeName}</legend>
+            <input value={this.props.v} onChange={function(e){
+                e.target.typeName = this.props.typeName;
+                this.props.onChange(e);
+            }.bind(this)}></input>
+        </fieldset>
+    }
+}
+
+class TempSet extends React.Component {
+    constructor(p) {
+        super(p);
+        this.state = {
+            t: ""
         }
     }
 
+    convertCtoF(v) {
+        return (v * 9 / 5) + 32;
+    }
+
+    conveertFtoC(v) {
+        return (v - 32) * 5 / 9;
+    }
+
+    handleChange(e) {
+        let v = e.target.typeName === "F" ? this.conveertFtoC(e.target.value) : e.target.value;
+        this.setState({t: v});
+    }
+
     render() {
-        return <fieldset>
-            <legend>biao ti</legend>
-            <input value={this.state.t} onChange={(e)=>this.setState({t:e.target.value})}></input>
-            <Warning t={parseFloat(this.state.t)}></Warning>
-        </fieldset>
+        return <div>
+            <TempInput typeName={"F"} v={this.convertCtoF(this.state.t)} onChange={this.handleChange.bind(this)}></TempInput>
+            <TempInput typeName={"C"} v={this.state.t} onChange={this.handleChange.bind(this)}></TempInput>
+        </div>
     }
 }
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <div>
-    <Caculat></Caculat>
-    </div>
+    <TempSet></TempSet>
 );
+
