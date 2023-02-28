f6c4b6f30ae5730335c94fb3b89bb719ea4752b4
lowkey <xinghao.wang@tosee.cn>
2023-02-28 09:00:54 +0000

    change context in chailed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const ContextValue = React.createContext("123");
+const ContextValue = React.createContext({
+    v:"111",
+    change:()=>{}
+});
 
 class ContextTest extends React.Component {
     constructor(p) {
         super(p);
         this.state = {
-            v: "111"
+            v: "111",
+            change: this.handleChange.bind(this)
         }
     }
 
+    handleChange() {
+        this.setState({
+            v:"222",
+            change: this.handleChange.bind(this)
+        });
+    }
+
     render() {        
-        return <ContextValue.Provider value={this.state.v}>
+        return <ContextValue.Provider value={this.state}>
         <div>
             <h1>如果不使用.Provider无法正常显示</h1>
             <h1>如果不设置value的值，context没有输出</h1>
@@ -46,7 +57,8 @@ class ContextLevel2 extends React.Component {
     render() {
         return <div>
             <h1>如果不设置contextType无法正常找到contex</h1>
-            <h1>{this.context}</h1>
+            <h1>{this.context.v}</h1>
+            <button onClick={this.context.change}>level2 change</button>
         </div>
     }
 }
