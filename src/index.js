2e8f1cec7faedcb70594a7a00d43673f61d8cafc
lowkey <xinghao.wang@tosee.cn>
2023-03-02 09:53:35 +0000

    first p element above H1

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import styel from "./style.css"
   let root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <body>
-
+    <p>这是标题前的第一个P元素</p>
     <h1>我是一级标题</h1>
-
+    <p>这是标题后的第一个P元素</p>
     <p>这是一个段落文本。在文本中有一个 <span class="special">span element</span>
 并且还有一个 <a href="http://example.com">链接</a>.</p>
 
