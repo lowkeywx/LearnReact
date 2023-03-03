8e07dfe71ee4a6f9f98b59089067ab6d8ed5f3f2
lowkey <xinghao.wang@tosee.cn>
2023-03-03 06:32:41 +0000

    所有指定类型的兄弟元素

--- src/index.js
+++ src/index.js
@@ -8,15 +8,17 @@ let liStyle = {
 let root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <body>
-    <ul>
-    <li>Unordered item</li>
-    <li>Unordered item
-        <ol>
-            <li>Item 1</li>
-            <li>Item 2</li>
-        </ol>
-    </li>
-</ul>
+        <ul>
+            <li>Unordered item1</li>
+            <li>Unordered item2</li>
+            <li>Unordered item3
+                <ol>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ol>
+            </li>
+        </ul>
 
-</body>
+    </body>
 );
