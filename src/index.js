7613dfe0ff0c87b7ebc86161dfa81c8605a72f30
lowkey <xinghao.wang@tosee.cn>
2023-03-03 07:03:20 +0000

    替代盒模型

--- src/index.js
+++ src/index.js
@@ -8,17 +8,9 @@ let liStyle = {
 let root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <body>
-        <ul>
-            <li>Unordered item1</li>
-            <li>Unordered item2</li>
-            <li>Unordered item3
-                <ol>
-                    <li>Item 1</li>
-                    <li>Item 2</li>
-                    <li>Item 3</li>
-                </ol>
-            </li>
-        </ul>
+        <div class="box">I use the standard box model.</div>
+        <div class="box1 alternate">I use the alternate box model.</div>
+
 
     </body>
 );
