c6133905b01abba51f22eb97efbc6685fb523a7b
lowkey <xinghao.wang@tosee.cn>
2023-03-03 06:23:57 +0000

    后代选择器

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import _ from "./style.css"
 let liStyle = {
     'background-color': "red"
 }
-  let root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
+let root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <body>
-    <p class="backendColor textColor content" >这是标题前的第一个P元素</p>
+    <p class="backendColor textColor content" >
+        这是标题前的第一个P元素 
+        <span>span element</span>
+    </p>
     <h1>我是一级标题</h1>
     <p class="special">这是一个段落文本。在文本中有一个 <span class="special">span element</span>
 并且还有一个 <a href="http://example.com">链接</a>.</p>
@@ -23,4 +26,4 @@ let liStyle = {
     </ul>
 
 </body>
-  );
+);
