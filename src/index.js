491d55fd9903b955b8cabd41e282db1dc2b8033a
lowkey <xinghao.wang@tosee.cn>
2023-03-03 06:28:06 +0000

    son element and jicheng

--- src/index.js
+++ src/index.js
@@ -8,22 +8,15 @@ let liStyle = {
 let root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <body>
-    <p class="backendColor textColor content" >
-        这是标题前的第一个P元素 
-        <span>span element</span>
-    </p>
-    <h1>我是一级标题</h1>
-    <p class="special">这是一个段落文本。在文本中有一个 <span class="special">span element</span>
-并且还有一个 <a href="http://example.com">链接</a>.</p>
-
-    <p>这是第二段。包含了一个 <em>强调</em> 元素。</p>
-    <p href="http://example.com">被p包裹的连接</p>
-    <p><a href="http://example.com">另一个被p包裹的连接</a></p>
     <ul>
-        <li>项目 1</li>
-        <li class="special">项目 2</li>
-        <li style={liStyle}>项目 <em>三</em></li>
-    </ul>
+    <li>Unordered item</li>
+    <li>Unordered item
+        <ol>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ol>
+    </li>
+</ul>
 
 </body>
 );
